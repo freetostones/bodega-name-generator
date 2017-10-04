7aa3f21d9b45b80ed2243aa79d24e5fc09355524
Michael C. Martinez <michaelmartinez2017@u.northwestern.edu>
2017-10-04 18:42:15 -0500

    Added a route for name generation

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import hashlib
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return render_template ("index.html")
+@app.route('/', methods=['GET'])
+def index():
+    return render_template ('index.html')
+
+@app.route('/generate/<name>')
+def generate(name):
+    hash_object = hashlib.md5(name.encode('utf8'))
+    value = int(hash_object.hexdigest(), 16)
+    if value % 2 == 0:
+        return 'Desus Nice'
+    else:
+        return 'The Kid Mero'
+
 
 if __name__ == "__main__":
     app.run()
