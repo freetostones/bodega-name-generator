23de20fd900223dec245fff0271be1cad5728f88
Michael C. Martinez <michaelmartinez2017@u.northwestern.edu>
2017-10-06 23:10:38 -0500

    Started working on form handling for name generation

--- app.py
+++ app.py
@@ -1,21 +1,29 @@
 from flask import Flask, render_template, request
+from wtforms import Form, StringField, validators
 import hashlib, csv
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+class NameForm(Form):
+    username = StringField('Name', [validators.Length(min=4, max=25)])
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template ('index.html')
 
-@app.route('/generate/<name>')
+@app.route('/generate/<name>', methods=['GET', 'POST'])
 def generate(name):
-    hash_object = hashlib.md5(name.encode('utf8'))
-    value = int(hash_object.hexdigest(), 16)
-    if value % 2 == 0:
-        return 'Desus Nice'
-    else:
-        return 'The Kid Mero'
+    form = NameForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.username.data
+        hash_object = hashlib.md5(name.encode('utf8'))
+        value = int(hash_object.hexdigest(), 16)
+        if value % 2 == 0:
+            return 'Desus Nice'
+        else:
+            return 'The Kid Mero'
 
+    return render_template('generate.html', form=form)
 
 if __name__ == "__main__":
     app.run()
